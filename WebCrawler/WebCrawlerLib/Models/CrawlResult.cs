7689ac3269469680bff033072729045c5995d8ce
Andrey <andrey17076@gmail.com>
2016-11-17 09:29:31 +0300

    Add Crawl results dummy.

---
+++ WebCrawler/WebCrawlerLib/Models/CrawlResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebCrawlerLib.Models
+{
+    public sealed class CrawlResult
+    {
+
+        public CrawlResult() : this(string.Empty)
+        {
+        }
+
+        public CrawlResult(string link) : this(link, new List<CrawlResult>())
+        {
+        }
+
+        public CrawlResult(string link, List<CrawlResult> nestedLinks)
+        {
+            Link = link;
+            NestedLinks = nestedLinks;
+        }
+
+        public string Link { get; set; }
+        public List<CrawlResult> NestedLinks { get; set; }
+    }
+}
