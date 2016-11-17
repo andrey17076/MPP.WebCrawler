8d0e768f1a623f5b2c12320ee68457f83996a6f5
Andrey <andrey17076@gmail.com>
2016-11-17 09:23:23 +0300

    Add confing and webcrawler models.

---
+++ WebCrawler/WebCrawler/Models/Config.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace WebCrawler.Models
+{
+    internal class Config
+    {
+        private const string ConfigPath = "config.xml";
+
+        private Config()
+        {
+        }
+
+        internal int Depth { get; set; }
+        internal string[] Urls { get; set; }
+
+        public static Config Load()
+        {
+            XDocument document = XDocument.Load(ConfigPath);
+            XElement root = document.Root;
+            return new Config
+            {
+                Depth = GetDepth(root),
+                Urls =  GetUrls(root)
+            };
+        }
+
+        private static int GetDepth(XContainer root)
+        {
+            try
+            {
+                return int.Parse(root.Element("depth").Value);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private static string[] GetUrls(XContainer root)
+        {
+            return root.Element("rootResources")?
+                .Elements("resource")
+                .Select(x => x.Value)
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+        }
+    }
+}
---
+++ WebCrawler/WebCrawler/Models/WebCrawler.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using WebCrawlerLib.Models;
+
+namespace WebCrawler.Models
+{
+    internal class WebCrawler
+    {
+        private readonly WebCrawlerLib.WebCrawler _webCrawler;
+
+        internal WebCrawler(int depth)
+        {
+            _webCrawler = new WebCrawlerLib.WebCrawler(depth);
+        }
+    
+        internal async Task<CrawlResult> GetCrawlResultAsync(string[] rootUrls)
+        {
+            return await _webCrawler.PerformCrawlingAsync(rootUrls);
+        }
+    }
+}
